ac88820b4c4b0bfb1a26cadfa19bbe8362ff49a6
hyun <edocnuyh@gmail.com>
2024-08-02 22:39:15 +0900

    chap02 - TDD시작 ~ 가독성정리, 폴더이동

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -2,16 +2,24 @@ package chap02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
+        // 암호가 null이거나 빈 문자열이면 암호 강도는 INVALID
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
+        // 충족하는 규칙 개수 산출
+        int metCounts = getMetCriteriaCounts(s);
+        // 충족하는 규칙 개수 1 이하 -> WEAK
+        if (metCounts <= 1) return PasswordStrength.WEAK;
+        // 충족하는 규칙 개수 2 -> NORMAL
+        if (metCounts == 2) return PasswordStrength.NORMAL;
+        // 그 외(충족 규칙 개수 2 초과) -> STRONG
+        return PasswordStrength.STRONG;
+    }
+
+    private static int getMetCriteriaCounts(String s) {
         int metCounts = 0;
-        // 개별 규칙을 검사하는 로직
         if (s.length() >= 8) metCounts++;
         if (meetsContainingNumberCriteria(s)) metCounts++;
         if (meetsContainingUppercaseCriteria(s)) metCounts++;
-
-        if (metCounts <= 1) return PasswordStrength.WEAK;
-        if (metCounts == 2) return PasswordStrength.NORMAL;
-        return PasswordStrength.STRONG;
+        return metCounts;
     }
 
     private static boolean meetsContainingUppercaseCriteria(String s) {
